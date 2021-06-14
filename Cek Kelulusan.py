e25718535be4ae1bd3d66020f20a2578315e1ea2
Iqbalfauzy2904 <85899825+Iqbalfauzy2904@users.noreply.github.com>
2021-06-15 01:25:22 +0700

    Pembahasan penggunaan If, else, elif, while

---
+++ Cek Kelulusan.py
@@ -0,0 +1,13 @@
+#cek kelulusan, jika nilai > 60 maka status lulus
+print ("==========================")
+print (" CEK KELULUSAN ")
+print ("==========================")
+#setiap value yang diinputkan, secara default bertipe data STRING
+n = input(">> Masukkan Nilai = ")
+#cek nilai
+if n>60:
+    sts = "LULUS"
+else:
+    sts="ULANG"
+
+print(sts)
